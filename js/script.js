bbcf6119a61b6f108d9deaafc52e4b1d33d06f50
Pedro Campanari <pedrocampanari09@hotmail.com>
2024-08-27 09:55:16 -0300

    Up

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ menuBurger.addEventListener('click', () => {
                 <div class="col-md-2 sidebar">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link active aside-a-style" href="/">Home</a>
+                            <a class="nav-link active aside-a-style" href="#top">Home</a>
                         </li>
                         <li class="nav-item ">
-                            <a class="nav-link aside-a-style" href="#">Sobre</a>
+                            <a class="nav-link aside-a-style" href="html/sobre.html">Sobre</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link aside-a-style" href="#">Projetos ECO</a>
